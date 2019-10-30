0417dae7c7304827266c45687785f35cb5f9e46d
Yaroslav Sushkov <alpex92@gmail.com>
2019-10-31 01:30:23 +0700

    using applicative

--- src/main/kotlin/com/alpex/domain/service/DeliveryService.kt
+++ src/main/kotlin/com/alpex/domain/service/DeliveryService.kt
@@ -3,11 +3,9 @@ package com.alpex.domain.service
 import arrow.Kind
 import arrow.core.Either
 import arrow.core.NonEmptyList
-import arrow.core.right
 import arrow.mtl.EitherT
 import arrow.mtl.extensions.eithert.monad.monad
 import arrow.mtl.fix
-import arrow.typeclasses.Applicative
 import arrow.typeclasses.Monad
 
 import com.alpex.domain.model.*
@@ -18,11 +16,9 @@ import com.alpex.request.CreateDeliveryRequest
 
 sealed class DeliveryServiceError
 data class ValidationError(val errors: NonEmptyList<String>): DeliveryServiceError()
-// TODO: Other error types if applicable?
 
 class DeliveryService<F>(
     private val M: Monad<F>,
-    private val A: Applicative<F>,
     private val deliveryRepo: DeliveryRepo<F>,
     private val userInfoRepo: UserRepo<F>
 ): Monad<F> by M {
@@ -37,21 +33,18 @@ class DeliveryService<F>(
             .mapLeft(::ValidationError)
 
         return ETM.fx.monad {
-            val (pair) =  EitherT.fromEither(A, deliveryAndUserInfoE)
+            val (pair) =  EitherT.fromEither(M, deliveryAndUserInfoE)
             val (delivery, userInfo) = pair
-            val (_) = EitherT.liftF<F, DeliveryServiceError, Unit>(M, deliveryRepo.add(delivery))
-            // TODO: Check before creating new user every time
-            val (_) = EitherT.liftF<F, DeliveryServiceError, Unit>(M, userInfoRepo.add(User(UserId.new(), userInfo)))
+            val f = M.tupled(deliveryRepo.add(delivery), userInfoRepo.add(User(UserId.new(), userInfo)))
+            val (_) = EitherT.liftF<F, DeliveryServiceError, Unit>(M, f.unit())
         }.fix().value()
-
     }
 
+    // TODO
+    /*
     fun details(deliveryId: DeliveryId): Kind<F, Unit?> {
-
         // TODO: Get Delivery, Point info (if needed), combine it?
-
     }
-
 
     // TODO: Not found
     fun changeStatus(id: DeliveryId, cmd: DeliveryCommand): Kind<F, Either<String, Unit>?> {
@@ -67,6 +60,6 @@ class DeliveryService<F>(
         }
 
         // TODO: Change status or return error
-
     }
+     */
 }
